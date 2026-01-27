e6fa63bd9b1460a284eac0db8d1dce80265c6865
HARVEY ORBISTA CLAUR <2302951@cc.usls.edu>
2026-01-27 14:30:16 +0800

    Lab Activity 2: Migration and Seeder

---
+++ database/seeders/SkillsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SkillsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('skills')->Insert([
+            ['name' => 'HTML', 'level' => 'Advanced'],
+            ['name' => 'CSS', 'level' => 'Intermediate'],
+            ['name' => 'JavaScript', 'level' => 'Intermediate'],
+            ['name' => 'PHP', 'level' => 'Advanced'],
+            ['name' => 'Laravel', 'level' => 'Beginner'],
+        ]);
+    }
+}
